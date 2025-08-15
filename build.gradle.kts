82e6c278ad7dfdac1c25dde853f7c7aa135c5034
meneguzzolucaa-coder <meneguzzolucaa@gmail.com>
2025-08-15 06:05:47 -0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version libs.versions.agp apply false
+    kotlin("android") version libs.versions.kotlin apply false
+}
